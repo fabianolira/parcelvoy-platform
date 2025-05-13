3e3a901e9567833c6b0d976460eaf6143bb5c81d
Fabiano Lira <fabianolira@gmail.com>
2025-05-13 20:15:15 -0300

    last fix

--- apps/ui/scripts/env.sh
+++ apps/ui/scripts/env.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Configuração para o frontend
 ENV_JS="./config.js"
-
 rm -rf ${ENV_JS}
 touch ${ENV_JS}
 
 varname='API_BASE_URL'
 value=$(printf '%s\n' "${!varname}")
 echo "window.$varname = \"$value\";" >> ${ENV_JS}
+
+# Substituir a variável API_BASE_URL na configuração do Nginx
+sed -i "s|\${API_BASE_URL}|$value|g" /etc/nginx/conf.d/default.conf
